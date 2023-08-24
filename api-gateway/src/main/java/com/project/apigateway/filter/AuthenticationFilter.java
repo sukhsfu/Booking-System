6f5947fd56c482351ba799b9885cc9a4d15a519a
sukhsfu <sukhwind@sfu.ca>
2023-08-23 18:48:47 -0700

    changed from feignCleint to webClient due to blocking(async) issue

--- api-gateway/src/main/java/com/project/apigateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/project/apigateway/filter/AuthenticationFilter.java
@@ -1,19 +1,18 @@
 package com.project.apigateway.filter;
 
-import com.project.apigateway.fiegnProxy.IdentityProxy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
 
-    @Lazy
     @Autowired
-    private IdentityProxy proxy;
+    private WebClient.Builder webClientBuilder;
 
     @Autowired
     private RouteValidator validator;
@@ -21,30 +20,35 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
         super(Config.class);
     }
 
-
-    @Override
-    public GatewayFilter apply(Config config) {
-        return (((exchange, chain) -> {
-            if(validator.isSecured.test(exchange.getRequest())){
-                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
-                    throw new RuntimeException("missing authorization header");
-                }
-                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-                if(authHeader != null && authHeader.startsWith("Bearer")){
-                    authHeader = authHeader.substring(7);
-                }
-                try{
-                    proxy.validateToken(authHeader);
-                }
-                catch (Exception e){
-                    throw new RuntimeException("Invalid token");
-                }
-            }
-            return chain.filter(exchange);
-        }));
-    }
-
     public static  class Config{
 
     }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            if (validator.isSecured.test(exchange.getRequest())) {
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("missing authorization header");
+                }
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                String[] parts = authHeader.split(" ");
+                if (parts.length != 2 || !"Bearer".equals(parts[0])) {
+                    throw new RuntimeException("Incorrect authorization structure");
+                }
+
+                return webClientBuilder.build()
+                        .get()
+                        .uri("lb://identityService/auth/validate?token=" + parts[1])
+                        .retrieve().bodyToMono(String.class)
+                        .map(String -> exchange).flatMap(chain::filter);
+
+            }
+            else{
+                return chain.filter(exchange);
+            }
+        };
+    }
+
+
 }
