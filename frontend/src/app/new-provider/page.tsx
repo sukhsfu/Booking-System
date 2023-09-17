c15ac12ee0ce83fa9fb2628cc67598705463e775
sukhsfu <sukhwind@sfu.ca>
2023-09-17 11:45:42 -0700

    [frontend]- added form 3 for new provider

--- frontend/src/app/new-provider/page.tsx
+++ frontend/src/app/new-provider/page.tsx
@@ -3,12 +3,18 @@
 import React, { useCallback, useState } from "react";
 import ProviderCredential from "./ProviderCredential";
 import ProviderInformation from "./ProviderInformation";
+import { useRouter } from "next/navigation";
+import ProviderLocation from "./ProviderLocation";
 
 const ProviderSignUp = () => {
+  const router = useRouter();
   const totalForms = 3;
   const [formPosition, setFormPosition] = useState(1);
   const onSubmitHandler = useCallback(() => {
     setFormPosition((preFormPosition) => {
+      if (preFormPosition === totalForms) {
+        router.push("1");
+      }
       return preFormPosition + 1;
     });
   }, []);
@@ -23,6 +29,12 @@ const ProviderSignUp = () => {
       )}
       {formPosition === 2 && (
         <ProviderInformation
+          onSubmit={onSubmitHandler}
+          formNumber={`${formPosition}/${totalForms}`}
+        />
+      )}
+      {formPosition === 3 && (
+        <ProviderLocation
           onSubmit={onSubmitHandler}
           formNumber={`${formPosition}/${totalForms}`}
         />
